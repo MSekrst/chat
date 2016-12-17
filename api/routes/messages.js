4b8d7dbed72ed15f0017b1f12d5e05c92db3d8b5
kmravunac <mrsckarlo@windowslive.com>
2016-12-17 16:01:09 +0100

    dodana ruta za novi conversation

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -9,8 +9,32 @@ messageRouter.get('/', authMiddleware.checkToken, (req, res) => {
   const db = getDb();
 
   const user = (req.user.username) ? (req.user.username) : (req.facebook.user.username);
-  const conversations = db.collection('conversations').find({ username: { $in: [user] } });
-  console.log(conversations);
+  db.collection('messages').find({ 'users.username': { $in: [user] } }).toArray((err, data) => {
+    if(err) {
+      console.log(err);
+      return res.status(500).json( { message: "err" } );
+    }
+
+    return res.status(200).json(data);
+  });
+});
+
+messageRouter.post('/init', authMiddleware.checkToken, (req, res) => {
+  const db = getDb();
+
+  let users = req.body.users;
+  users.push(req.user);
+
+  db.collection('messages').insertOne({
+    "title": req.body.title,
+    "users": users,
+    "message": []
+  }).then(conversation => {
+    return res.status(201).json(conversation);
+  }).catch( err => {
+    console.log(err);
+    return res.status(500).json( { message: "err" } );
+  });
 });
 
 export default messageRouter;
