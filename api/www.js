925d323206e65ea5bcb109dcab66d95de68f8fcb
Josipa Kelava <kelava.josipa@gmail.com>
2016-12-17 18:38:11 +0100

    socketi dodani

--- api/www.js
+++ api/www.js
@@ -4,10 +4,14 @@ import debug from 'debug';
 import http from 'http';
 
 import app from './app';
+import socketio from 'socket.io';
 
 /**
  * Normalize a port into a number, string, or false.
  */
+
+const userSocket = [];
+
 function normalizePort(val) {
   const port = parseInt(val, 10);
 
@@ -58,6 +62,31 @@ function onError(error) {
 }
 
 const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection',
+  (socket) => {
+
+    socket.on('user', function(msg){
+      const newUserSocket = {
+        "user": msg,
+        "socket": socket
+      }
+      userSocket.push(newUserSocket);
+      console.log("blabla");
+      console.log((userSocket));
+    });
+
+    socket.on('disconnect',
+      ()=> {
+        for(var i = 0; i < userSocket.length; i++) {
+          if(userSocket[i].socket == socket) {
+            works.splice(i, 1);
+          }
+        }
+        console.log(userSocket);
+      });
+});
 
 /**
  * Event listener for HTTP server "listening" event.
@@ -77,3 +106,7 @@ console.log('Server is running!');
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
+
+export const getConnected = function () {
+  return userSocket;
+};
