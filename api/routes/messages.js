31873cd9b158fedbd17516ac372859c26cda4d42
Josipa Kelava <kelava.josipa@gmail.com>
2016-12-17 14:19:30 +0100

    dohvati razgovor

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -4,12 +4,26 @@ import { getDb } from '../mongo';
 
 const messageRouter = express.Router();
 
-messageRouter.get('/', (req, res) => {
-  const db = getDb();
+messageRouter.get('/',
+  (req, res) => {
+    const db = getDb();
 
-  const user = (req.user.username) ? (req.user.username) : (req.facebook.user.username);
-  const conversations = db.collection('conversations').find({ username: { $in: [user] } });
-  console.log(conversations);
-});
+    const user = (req.user.username) ? (req.user.username) : (req.facebook.user.username);
+    const conversations = db.collection('conversations').find({ username: { $in: [user] } });
+    console.log(conversations);
+  }
+);
 
+messageRouter.get('/:title',
+  (req, res) => {
+    const db = getDb();
+
+    const sender = (req.user.username) ? (req.user.username) : (req.facebook.user.username);
+    const title = req.param.title;
+
+    const conversation = db.collection('conversation').findOne({title: title});
+    console.log(conversation);
+
+  }
+);
 export default messageRouter;
