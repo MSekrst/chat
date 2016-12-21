794188dbee7dedc68f61b511d332dfea6086a31e
teapovic <teapovic@gmail.com>
2016-12-21 18:19:19 +0100

    login i registracija

--- Android/app/src/main/java/eu/rasus/fer/chat/ChatsPreview/AllChatsPreviewFragment.java
+++ Android/app/src/main/java/eu/rasus/fer/chat/ChatsPreview/AllChatsPreviewFragment.java
@@ -20,6 +20,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnItemClick;
+import eu.rasus.fer.chat.Application;
 import eu.rasus.fer.chat.Chat.ChatActivity;
 import eu.rasus.fer.chat.HttpsConstants;
 import eu.rasus.fer.chat.R;
@@ -58,7 +59,7 @@ public class AllChatsPreviewFragment extends Fragment {
     Retrofit retrofit = new Retrofit.Builder().baseUrl(HttpsConstants.ADDRES).client(HttpsConstants.getUnsafeOkHttpClient()).addConverterFactory(GsonConverterFactory.create(gson)).build();
     RestApi api = retrofit.create(RestApi.class);
 
-    Call<List<ChatPreviewItem>> call = api.getAllMessages();
+    Call<List<ChatPreviewItem>> call = api.getAllMessages(Application.TOKEN);
 
     call.enqueue(new Callback<List<ChatPreviewItem>>() {
 
@@ -67,8 +68,8 @@ public class AllChatsPreviewFragment extends Fragment {
         chatPreviewAdapter = new ChatPreviewAdapter(getActivity(), response.body());
 
         itemContainer.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        if (response.body().size()==0) noChats.setVisibility(View.VISIBLE);
         itemContainer.setAdapter(chatPreviewAdapter);
-        itemContainer.setEmptyView(noChats);
       }
 
       @Override
@@ -83,7 +84,7 @@ public class AllChatsPreviewFragment extends Fragment {
   @OnItemClick(R.id.chat_item_container)
   public void openChat(final AdapterView<?> adapter, final View view, final int position, final long id) {
     Intent intent = new Intent(getActivity(), ChatActivity.class);
-    intent.putExtra("SENDER", HttpsConstants.ME);
+    intent.putExtra("SENDER", Application.USERNAME);
     intent.putExtra("CHAT_PREVIEW_ITEM", ((ChatPreviewItem)adapter.getItemAtPosition(position)));
     startActivity(intent);
   }
---
+++ Android/app/src/main/java/eu/rasus/fer/chat/Login/SplashPresenter.java
@@ -0,0 +1,46 @@
+package eu.rasus.fer.chat.Login;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+
+import eu.rasus.fer.chat.MainActivity;
+
+public class SplashPresenter {
+  SplashView mSplashView;
+
+  public SplashPresenter(SplashView splashView) {
+    mSplashView = splashView;
+  }
+
+  public void startApp() {
+    SharedPreferences prefs = mSplashView.getSharedPreferences("data", Context.MODE_PRIVATE);
+    String username = prefs.getString("USERNAME", "");
+    String token = prefs.getString("TOKEN", "");
+
+    Intent intent;
+//    if (username.isEmpty() || token.isEmpty()) {
+     intent = new Intent(mSplashView, LoginView.class);
+
+//    }
+//    else {
+//      eu.rasus.fer.chat.Application.USERNAME =  username;
+//      eu.rasus.fer.chat.Application.TOKEN = token;
+//      intent = new Intent(mSplashView, MainActivity.class);
+//    }
+
+    mSplashView.startActivity(intent);
+    mSplashView.finish();
+  }
+
+  public void mockWait(){
+    Handler handler = new Handler();
+    handler.postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        startApp();
+      }
+    }, 2000);
+  }
+}
