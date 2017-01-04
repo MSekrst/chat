f185101a69440669431b84db8bdb02dbe43485aa
bAndelic <bruna.andelic@fer.hr>
2017-01-04 23:15:13 +0100

    redizajn

--- src/list/Item.jsx
+++ src/list/Item.jsx
@@ -2,15 +2,16 @@ import React, {Component} from 'react';
 
 class Item extends Component {
   render() {
+    // slika src this.props.picture a tek ako nema treba placeholder - staviti to
     return (
       <div>
         <div className="item" onClick={this.props.click}>
           <div className="imageItem">
-            <img src={this.props.picture} className="img-circle profileImage" alt="Cinque Terre"
+            <img src="./images/profilePlaceholder.jpg" className="img-circle profileImage" alt="Cinque Terre"
                  width="50px" height="50px"/>
           </div>
           <div className="nameItem">
-            {this.props.name}
+            {this.props.name.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase()+ txt.substr(1)})}
           </div>
         </div>
       </div>
--- src/list/List.jsx
+++ src/list/List.jsx
@@ -4,8 +4,11 @@ import Item from './Item.jsx';
 class List extends Component {
   render() {
     return (
-      <div id="list">
-        {this.props.messages.map(m => { return <Item key={m._id} name={m.title}  click={() => this.props.click(m._id)} /> })}
+      <div id="talkList">
+        <div id="newTalk"><span className="glyphicon glyphicon-plus navIcon" />&nbsp;&nbsp;Add new talk</div>
+        <div id="list">
+          {this.props.messages.map(m => { return <Item key={m._id} name={m.title}  click={() => this.props.click(m._id)} /> })}
+        </div>
       </div>
     );
   }
