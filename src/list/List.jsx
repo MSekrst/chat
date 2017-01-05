28d313b27b364975070cd96a12605604409c0367
bAndelic <bruna.andelic@fer.hr>
2017-01-05 21:13:50 +0100

    sredeno i dodan modal za nove razgovore

--- src/list/List.jsx
+++ src/list/List.jsx
@@ -5,9 +5,26 @@ class List extends Component {
   render() {
     return (
       <div id="talkList">
-        <div id="newTalk"><span className="glyphicon glyphicon-plus navIcon" />&nbsp;&nbsp;Add new talk</div>
-        <div id="list">
+        <button id="newTalk" type="button" data-toggle="modal" data-target="#myModal">
+          <div ><span className="glyphicon glyphicon-plus talkIcon" />&nbsp;&nbsp;Add new talk</div>
+        </button>
+          <div id="list">
           {this.props.messages.map(m => { return <Item key={m._id} name={m.title}  click={() => this.props.click(m._id)} /> })}
+        </div>
+        <div className="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 className="modal-title" id="myModalLabel"><span className="glyphicon glyphicon-search talkIcon" />&nbsp;&nbsp;&nbsp;&nbsp;Find friend</h4>
+              </div>
+              <div className="modal-body">
+                ...
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
